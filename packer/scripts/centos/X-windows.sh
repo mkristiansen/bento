a43d0cd4c54f5ec33b23815c9b829d49fd64651e
Morten Kristiansen <morten.kristainsen@gmail.com>
2015-09-02 19:36:46 +0100

    added -y on yum group install. Moved x-windows install script to before vmtools.sh

--- packer/scripts/centos/X-windows.sh
+++ packer/scripts/centos/X-windows.sh
@@ -2,7 +2,7 @@
 
 if [[ "$X_WINDOWS" == "true" ]]; then
   echo 'Installing X-Windows system and Gnome Desktop'
-  yum groupinstall "Server with GUI"
+  yum groupinstall "Server with GUI" -y
   
 else
   echo 'Not installing X-Windows system'
